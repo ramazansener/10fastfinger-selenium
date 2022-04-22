bb2d3bdb82d0fa59d63c3cd42f42431511413a4c
Ramazan SENER <ramazan.sener6507@gmail.com>
2022-04-23 00:46:26 +0300

    10fastfingers task done

--- src/test/main.java
+++ src/test/main.java
@@ -16,29 +16,13 @@ public class main {
 		driver.get("https://10fastfingers.com/typing-test/turkish");
 		driver.manage().window().maximize();
 		
-		/*
-		driver.findElement(By.className("modal-close")).click();
-		driver.findElement(By.className("close-button")).click();
-		driver.findElement(By.xpath("//*[@id=\"auto-complete-app\"]/div/div/input")).click();
-
-		
-
-		WebElement searchBox =driver.findElement(By.xpath("//*[@id=\"auto-complete-app\"]/div/div/input"));
-		searchBox.click();
-		searchBox.sendKeys("kulaklýk");
-		Thread.sleep(2000);
-		WebElement searchIcon =driver.findElement(By.xpath("//*[@id=\"auto-complete-app\"]/div/div/i"));
-		searchIcon.click();*/
 		
 		Thread.sleep(3000);
 		
 		
-		
 		WebElement inputField =driver.findElement(By.id("inputfield"));
 		
-		
 		List<WebElement> wordList=driver.findElements(By.xpath("//*[@id=\"row1\"]/span"));
-		
 		
 		
 		for (int i=0;i<wordList.size();i++) {
